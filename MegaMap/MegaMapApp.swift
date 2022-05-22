73188fb99638ef8ade70d2274e62ab4d28edd217
Amedeo Cristllo <>
2022-05-21 20:01:18 -0700

    PUSH

---
+++ MegaMap/MegaMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaMapApp.swift
+//  MegaMap
+//
+//  Created by Amedeo Cristillo on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MegaMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
